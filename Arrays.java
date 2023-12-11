ad65055a5e3448fdeccf4686d7c4497a9c01934a
yeabsira_mesfin <yabidesta@gmail.com>
2023-12-11 06:45:03 -0500

    arrays

---
+++ Arrays.java
@@ -0,0 +1,17 @@
+class Arrays{
+    public static void main(String[] args){
+    //   int[] numbers = {11,2,30,4,5};
+    //   System.out.println(java.util.Arrays.toString(numbers));
+    //   java.util.Arrays.sort(numbers);
+    //   System.out.println(numbers.length);
+    //   System.out.println(java.util.Arrays.toString(numbers));
+    //   System.out.println(numbers[4]);
+    // Multidimensional arrays.
+    int[][] multi= new int[3][3];
+    int[][] mulitie = {{2,3,5},{4,44,5},{2,3,3,5}};
+    multi[2][2] = 1;
+         System.out.println(java.util.Arrays.deepToString(multi));
+         System.out.println(java.util.Arrays.deepToString(mulitie));
+
+    }
+}  
