a3892050747f1d83a834fcdaad8e4ee74a75a2db
yeabsira_mesfin <yabidesta@gmail.com>
2023-12-11 05:55:33 -0500

    strings

---
+++ Strings.java
@@ -0,0 +1,13 @@
+class Strings{
+    public static void main(String[] args){
+        String name = "    Yeabsira_Mesfin " + "Be \"Tough !! \" \n you will be soon   ";
+        System.out.println(name);
+        System.out.println(name.startsWith("Yeab"));
+        System.out.println(name.indexOf("b"));
+        System.out.println(name.replace("Be", "You are"));
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.trim());
+        // Escape Sequences
+    }
+}  
