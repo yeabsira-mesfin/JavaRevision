78b46cf7b3b278f0a2170320b5e906881587cb26
yeabsira_mesfin <yabidesta@gmail.com>
2023-12-15 04:42:26 -0500

    JavaRevision

---
+++ NumberFormatting.java
@@ -0,0 +1,18 @@
+import java.text.NumberFormat;
+
+/**
+ * NumberFormatting
+ */
+public class NumberFormatting {
+
+    public static void main(String[] args) {
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String result = currency.format(12345679.287);
+        System.out.println(result);
+
+
+      
+        String resultPercent = NumberFormat.getPercentInstance().format(.008);
+        System.out.println(resultPercent);
+    }
+}
