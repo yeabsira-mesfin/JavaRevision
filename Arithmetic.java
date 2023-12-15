8fc533339f1ce0c339d4b6926edcd1fdb29f041b
yeabsira_mesfin <yabidesta@gmail.com>
2023-12-15 03:56:20 -0500

    Arithmetic Expressions

---
+++ Arithmetic.java
@@ -0,0 +1,17 @@
+/**
+ * Arithmetic
+ */
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        double result = (double)10/(double)3;
+        int x =1;
+        x++;
+        int y = x++;
+        int z=++x;
+        System.out.println(z);
+        System.out.println(y);
+        System.out.println(x);
+        System.out.println(result);
+    }
+}
