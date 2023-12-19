a70e2232a8718e75210b5778943f5ce0bc0d946b
yeabsira_mesfin <yabidesta@gmail.com>
2023-12-19 05:28:36 -0500

    Ternary_Operator

---
+++ TernaryOperator.java
@@ -0,0 +1,12 @@
+/**
+ * TernaryOperator
+ */
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+         int temprature = 20;
+         String isNum = temprature > 20 ? "The temprature is normal" : "The temprature is cold";
+         System.out.println(isNum);
+        
+    }
+}
