3ed362122b6a12932645c4c4c38b7f7fd9cc0a65
yeabsira_mesfin <yabidesta@gmail.com>
2023-12-19 05:37:04 -0500

    Switch

---
+++ Switch.java
@@ -0,0 +1,21 @@
+/**
+ * Switch
+ */
+public class Switch {
+
+    public static void main(String[] args) {
+        String role = "user";
+        switch (role) {
+            case "admin":
+                System.out.println("Your role is admin");
+                break;
+            case "user":
+            System.out.println("You are a user");
+               break;
+            default:
+            System.out.println("Invalid login");
+                break;
+        }
+
+    }
+}
