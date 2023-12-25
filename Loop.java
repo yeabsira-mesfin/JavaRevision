2c6a76beff2107001d83747499c6b05eac81e039
yeabsira_mesfin <yabidesta@gmail.com>
2023-12-25 05:29:32 -0500

    Loop

---
+++ Loop.java
@@ -0,0 +1,10 @@
+/**
+ * FoorLoop
+ */
+public class Loop {
+
+    public static void main(String[] args) {
+        for(int i = 0; i<5;i++)
+        System.out.println("Looping five times.");
+    }
+}
