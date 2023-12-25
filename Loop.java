60d6e0fb6d65a10445d0f686b26c562d1cb01eea
yeabsira_mesfin <yabidesta@gmail.com>
2023-12-25 05:54:45 -0500

    do...while

--- Loop.java
+++ Loop.java
@@ -10,11 +10,16 @@ public class Loop {
         // System.out.println("Looping five times:" + i);\
         Scanner scanner = new Scanner(System.in);
         String input = "";
-        while(!input.equals("quit")){
-            System.out.println("Please enter you user name");
-             input = scanner.nextLine().toLowerCase();
-             System.out.println(input);
+        // while(!input.equals("quit")){
+        //     System.out.println("Please enter your user name");
+        //      input = scanner.nextLine().toLowerCase();
+        //      System.out.println(input);
+        // }
+        do{
+            System.out.println("Please enter your user name");
+            input = scanner.nextLine().toLowerCase();
+            System.out.println(input);
         }
-        
+        while(!input.equals("quit"));
         }
 }
