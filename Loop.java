d97ad1977271a7f5c044fe891053ae51a940a43a
yeabsira_mesfin <yabidesta@gmail.com>
2023-12-25 05:45:38 -0500

    while

--- Loop.java
+++ Loop.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 /**
  * FoorLoop
  */
 public class Loop {
 
     public static void main(String[] args) {
-        for(int i = 0; i<5;i++)
-        System.out.println("Looping five times.");
-    }
+        // for(int i = 1; i<=5;i++)
+        // System.out.println("Looping five times:" + i);\
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while(!input.equals("quit")){
+            System.out.println("Please enter you user name");
+             input = scanner.nextLine().toLowerCase();
+             System.out.println(input);
+        }
+        
+        }
 }
