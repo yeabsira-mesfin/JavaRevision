529a10e83164f7a91ac5feed83c16ef25cef3205
yeabsira_mesfin <yabidesta@gmail.com>
2023-12-25 06:09:39 -0500

    for...each

--- Loop.java
+++ Loop.java
@@ -15,16 +15,27 @@ public class Loop {
         //      input = scanner.nextLine().toLowerCase();
         //      System.out.println(input);
         // }
-        do{
-            System.out.println("Please enter your user name");
-            input = scanner.nextLine().toLowerCase();
-            // if(!input.equals("quit"))
-            if(input.equals("pass"))
-            continue;
-             if(input.equals("quit"))
-             break;
-            System.out.println(input);
+        // do{
+        //     System.out.println("Please enter your user name");
+        //     input = scanner.nextLine().toLowerCase();
+        //     // if(!input.equals("quit"))
+        //     if(input.equals("pass"))
+        //     continue;
+        //      if(input.equals("quit"))
+        //      break;
+        //     System.out.println(input);
+        // }
+        // while(true);
+        
+        String[] fruits = {"Mango","Apple","Banana"};
+        // for(int i=0;i<fruits.length;i++){
+        //     System.out.println(fruits[i]);
+        // }
+        // For each loop has few limitations, such as it is forward where you can't iterate back ward
+        // you don't have access to the index.
+        for(String fruit: fruits){
+            System.out.println(fruit);
         }
-        while(true);
+
         }
 }
